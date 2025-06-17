25318ada1ead4738bfc5feaeb1f0658775bbc8e6
Steven C Scott <steven.scott.dev@gmail.com>
2025-06-17 15:38:52 -0400

    Update Signup.js

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -14,7 +14,7 @@ const Signup = ({ onAuth }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/users/signup', form);
+      await axios.post(`${process.env.REACT_APP_API_URL}/users/signup`, form);
       localStorage.setItem('token', res.data.token);
       onAuth(res.data.user);
     } catch (err) {
